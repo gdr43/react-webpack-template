71acada9a2bf965624d0415c9791ea75c26bf120
dilipgujju <dgujju@gemini-us.com>
2019-08-25 16:26:03 +0530

    First commit

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div>This is the App</div>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/,
+        use:
+          {
+            loader: "html-loader"
+          }
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
